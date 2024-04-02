5d1db62e55c61ff5fb0055b494f3c7ee4bd8288a
Vinodhinichokkan <vinodhinichokkan99@gmail.com>
2024-04-02 11:14:29 +0530

    exe

---
+++ color.js
@@ -0,0 +1,7 @@
+
+function changeColor(){
+
+    let but_Ref=document.getElementById('abc');
+  
+    but_Ref.style.backgroundColor="blue"
+}
---
+++ one.js
@@ -0,0 +1,16 @@
+let emp={ id:101,
+    name:'Rahul Gandhi'
+  }  
+
+let user={
+
+}
+console.log(Object.keys(emp))
+console.log(Object.keys(emp).length)
+console.log(Object.keys(emp).length > 0)
+
+console.log("*************")
+
+console.log(Object.keys(user))
+console.log(Object.keys(user).length)
+console.log(Object.keys(user).length > 0)
